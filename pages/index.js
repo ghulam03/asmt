5446abbd43671eefdde03f9f3891ecaa58fdede3
ghulam03 <ghulam8905@gmail.com>
2023-03-05 14:11:27 +0530

    made progress in app

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
+import React, { useState } from 'react'
+
 import Link from 'next/link'
-import React from 'react'
 import prisma from '../prisma/prisma'
 import { useSelector } from 'react-redux'
+
 function Index(props) {
+    const [persons, setpersons] = useState(props.persons)
     
 
     const isAuth=useSelector((state)=>state.user.isAuthenticated)
+    function deleteHandler(){
+        
+    }
   return (
   <>
   {isAuth && (
@@ -35,9 +41,30 @@ function Index(props) {
     </div>
     </>
   )}
-  {/* {isAuth && (
+  {isAuth && (
+    persons.map((c) => {
+        return (
+          <>
+              <div className='  ml-56  bg-yellow-300'>
+        <tr className=' flex flex-row justify-between'>
+            <td>{c.uname}</td>
+            <td>{c.email}</td>
+            <td>{c.phone}</td>
+            <td>{c.company}</td>
+            <td>
+                <span>
+                    <button>Edit</button>
+                    <button onClick={deleteHandler}>Delete</button>
+                </span>
+            </td>
+        </tr>
+        
 
-  )} */}
+        </div>
+          </>
+
+  )})
+  )}
   
   </>
   )
